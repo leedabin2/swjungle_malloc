85497e0ad47f09a04924b119ac8fb0b9f38f149d
leedabin2 <bindalee02@gmail.com>
2024-04-13 02:22:07 +0000

    feat: 상수 매크로 정의

--- mm.c
+++ mm.c
@@ -38,7 +38,13 @@ team_t team = {
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
+
 /* 가용리스트 조작을 위한 기본 상수 및 매크로 정의 */
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) // size보다 큰 가장 가까운 ALIGN 의 배수로 만들어 줌 (정)
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 8바이트
 
 // 기본 size 상수 정의
 #define WSIZE 4 // 워드 크기
@@ -48,31 +54,31 @@ team_t team = {
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 
 // 헤더와 푸터에 저장하는 값을 리턴하는 매크로
-#define PACk (size,alloc) ((size) | (alloc)) // (0-free, 1-alloc 삽입)
+#define PACk(size,alloc) ((size) | (alloc)) // (0-free, 1-alloc 삽입)
 
 // GET - 주소 P가 참조하는 워드를 읽는 함수
-#define GET(p)
+#define GET(p) (*(unsigned int*) (p))
 // PUT - 주소 P가 가리키는 워드에 val을 저장하는 함수
-#define PUT(p,val)
+#define PUT(p,val) (*(unsigned int*)(p) = (val)) // void형을 int로변환 후 역참조
 
-// GET-SIZE - 주소 P가 기리키는 워드의 사이즈 정보를 가져오는 함수
-#define GET-SIZE(p)
+// GET-SIZE - 주소 P가 가리키는 워드의 사이즈 정보를 가져오는 함수
+#define GET_SIZE(p) (GET(p) & ~0x7) // 블록의 크기가 8의 배수로 정렬되어 있기 때문
 // GET-ALLOC - 주소 P가 가리키는 워드의 a(할당여부)를 가져오는 함수
-#define GET-ALLOC(p)
+#define GET_ALLOC(p) (GET(p) & 0x1)
 
 // HDRP - bp가 속한 블록의 헤더를 알려주는 함수
-#define HDRP(bp)
+#define HDRP(bp) ((char*)(bp) - WSIZE)
 // FTRP - bp가 속한 블록의 푸터를 알려주는 함
-#define FTRT(bp)
+#define FTRT(bp) ((char*)(bp) + GET_SIZE(HDRP(bp) - DSIZE))
 
 // 블록 포인터를 인자로 받아서 이후와 이전 블록 주소를 리턴
-#define NEXT_BLKP(bp)
-#define PREV_BLKP(bp) 
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+#define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) // size보다 큰 가장 가까운 ALIGN 의 배수로 만들어 줌 (정)
+// #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) // size보다 큰 가장 가까운 ALIGN 의 배수로 만들어 줌 (정)
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 8바이트
+// #define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // 8바이트
 /*--------------------------------------------------*/
 
 /* 
@@ -80,6 +86,14 @@ team_t team = {
  초기 가용영역 4워드를 설정해줌 에러시 -1 반
  */
 static char* heap_listp;
+
+int mm_init(void);
+void *mm_malloc(size_t size);
+void mm_free(void *bp);
+void *mm_realloc(void *ptr,size_t size);
+
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
 
 int mm_init(void)
 {
@@ -92,7 +106,8 @@ int mm_init(void)
 
     heap_listp += (2*WSIZE);
 
-    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) return -1;
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) 
+        return -1;
 
     return 0;
 }
@@ -101,8 +116,9 @@ static void *extend_heap(size_t words) {
     char *bp; // 블록포인터
     size_t size; // size 는 힙의 총 바이트 수
 
-    size = (words % 2 == 1) ? ( (words + 1) * WSIZE ) : size * WSIZE;
-    if ( (bp = mem_sbrk(size) ) == -1) return NULL; // 새메모리의 첫 부분을 bp로 지정
+    size = (words % 2 == 1) ? ( (words + 1) * WSIZE ) : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size) ) == -1) 
+        return NULL; // 새메모리의 첫 부분을 bp로 지정
 
     // 들어오는 가용 블록의 헤더와 푸터 지정 
     PUT(HDRP(bp),PACk(size,0));
@@ -110,10 +126,9 @@ static void *extend_heap(size_t words) {
     PUT(HDRP(NEXT_BLKP(bp)),PACk(0,1));
 
     // 이전 블록이 가용이면 연결
-    return 0;
+    return coalesce(bp);
 
 }
-
 
 
 /* 
@@ -135,10 +150,62 @@ void *mm_malloc(size_t size)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    // 주소값의 사이즈의 정보를 가져옴
+    size_t size = GET_SIZE(bp);
+
+    PUT(HDRP(bp),PACk(size,0));
+    PUT(FTRT(bp),PACk(size,0));
+    
+    coalesce(bp);
+
 }
 
+// free 블록 반환 후 경계 태그 연결사용해서 상수시간에 인접 가용블록과 통합
+static void *coalesce(void *bp) {
+    // (헤더) 이전 할당 여부 사이즈 - 이전에 할당한 여부를 헤더의 주소에서 가져오기
+    size_t prev_alloc = GET_ALLOC(FTRT(PREV_BLKP(bp)));
+    // (푸터) 이후 할당 여부 사이즈 - 이후에 할당한 여부를 푸터의 주소에서 가져오기
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    // 현재 반환할 주소
+    size_t curr_size = GET_SIZE(HDRP(bp));
+
+    // case 1 -> 이전, 이후 다음 블록이 모두 할당된 경우
+    if (prev_alloc && next_alloc) { 
+        return bp;
+    }
+
+    // case 2 -> 이전은 할당 다음은 가용
+    else if (prev_alloc && !next_alloc) {
+         curr_size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        // 현재 블록의 헤더는 현재 + 다음 블록의 크기를 합한 것
+        PUT(HDRP(bp),PACk(curr_size,0));
+        // 다음 블록의 푸터는 현재 + 다음 블록의 크기를 합한 것
+        PUT(FTRT(bp),PACk(curr_size, 0));
+  
+    }
+
+    // case 3 -> 이전은 가용 다음은 할당
+    else if (!prev_alloc && next_alloc) {
+        curr_size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+
+        PUT(FTRT(bp),PACk(curr_size,0));
+        PUT(HDRP(PREV_BLKP(bp)),PACk(curr_size,0));
+        bp = PREV_BLKP(bp);
+
+    }
+
+    // case 4 -> 모두 가용 
+    else {
+        curr_size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACk(curr_size,0));
+        PUT(FTRT(NEXT_BLKP(bp)),PACk(curr_size,0));
+        bp = PREV_BLKP(bp);
+
+    }
+    return bp;
+}
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
